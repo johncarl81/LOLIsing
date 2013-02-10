8a28c331b239f781c0e00de354296268758f2154
John Ericksen <johncarl81@gmail.com>
2013-02-10 12:14:33 -0700

    Using bitwise operators for more resolution

--- ising.ino
+++ ising.ino
@@ -1,19 +1,15 @@
 #include <Charliplexing.h>   
 
-#define DELAY 10
-#define RESEEDRATE 10000
+#define DELAY 0
+#define RESEEDRATE 100000
 #define SCREENX 14
 #define SCREENY 9
-#define SCALEX 2
-#define SCALEY 2
-#define SIZEX 28
-#define SIZEY 18
-/*
+#define SCALEX 4
+#define SCALEY 6
 #define SIZEX SCREENX * SCALEX
 #define SIZEY SCREENY * SCALEY
-*/
 
-byte world[SIZEX][SIZEY];
+byte worldArray[SCREENX * SIZEY];
 long density = 50;
 int loopvar = 0;
 double shadesScale = (SCALEX * SCALEY) * 1.0 / SHADES;
@@ -29,26 +25,30 @@ void setup() {
 
 void loop() {
   
+  for(int i = 0; i < 100; i++){
+  
   int x = random(SIZEX);
   int y = random(SIZEY);
   
   double neighbors = 0;
   
-  neighbors += map0neg(world[wrap((x + 1), SIZEX)][y]);
-  neighbors += map0neg(world[wrap((x - 1), SIZEX)][y]);
-  neighbors += map0neg(world[x][wrap((y + 1), SIZEY)]);
-  neighbors += map0neg(world[x][wrap((y - 1), SIZEY)]);
+  neighbors += map0neg(world(wrap((x + 1), SIZEX), y));
+  neighbors += map0neg(world(wrap((x - 1), SIZEX), y));
+  neighbors += map0neg(world(x, wrap((y + 1), SIZEY)));
+  neighbors += map0neg(world(x, wrap((y - 1), SIZEY)));
   
   double prob = exp(neighbors) / (exp(-neighbors) + exp(neighbors));
   
   if(prob > (random(1000) / 1000.0)){
-    world[x][y] = 1;
+    setWorld(x, y, 1);
   }
   else{
-    world[x][y] = 0;
+    setWorld(x, y, 0);
+  }
   }
   
-  update(x,y);
+  update();
+  //update(x,y);
   
   //Counts and then checks for re-seeding
   //Otherwise the display will die out at some point
@@ -57,17 +57,19 @@ void loop() {
     seedWorld();
     loopvar = 0;
   }
+  
   delay(DELAY);
 }
 
 void update(int x, int y){
   int scaledx = x / SCALEX;
   int scaledy = y / SCALEY;
+  
  
   int total = 0;
   for(int i = 0; i < SCALEX; i++){
     for(int j = 0; j < SCALEY; j++){
-      total += world[scaledx + i][scaledy + j];
+      total += world(scaledx + i, scaledy + j);
     }
   }
  
@@ -95,17 +97,37 @@ void seedWorld(){
   for (int i = 0; i < SIZEX; i++) {
     for (int j = 0; j < SIZEY; j++) {
       if (random(100) < density) {
-        world[i][j] = 1;
+        setWorld(i, j, 1);
       }
       else {
-        world[i][j] = 0;
+        setWorld(i, j, 0);
       }
     }
   }
   
-  for (int i = 0; i < SIZEX; i++) {
-    for (int j = 0; j < SIZEY; j++) {
-      update(i,j);
+  update();
+}
+
+void update(){
+  for (int i = 0; i < SCREENX; i++) {
+    for (int j = 0; j < SCREENY; j++) {
+      update(i * SCALEX,j * SCALEY);
     }
   }
+}
+
+byte setWorld(int x, int y, byte input){
+  
+  int xRef = x / 4;
+  int xBit = x - (4 * (x / 4));
+  
+  bitWrite(worldArray[(xRef * SIZEY) + y], xBit, input);
+}
+
+byte world(int x, int y){
+  
+  int xRef = x / 4;
+  int xBit = x - (4 * (x / 4));
+  
+  return bitRead(worldArray[(xRef * SIZEY) + y], xBit);
 }
